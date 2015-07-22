009b08800038c227a43b8cc42c034dd42997efb0
HiranoKa <Hirano.katsumi@leadinge.co.jp>
2015-07-22 18:41:08 +0900

    3以上の自然数は判別できるver

--- prime.py
+++ prime.py
@@ -1,28 +1,14 @@
 __author__ = 'le-user'
 import sys
 # -*- coding: utf-8 -*-
-#input=sys.stdin.readline()
-#n=int(input)
-q=19
-if pow(2,q-1,q)==1:
-    print("True")
-else:
-    print("False")
-
-
+n=2
 list=range(2,n)
-amari=[]
+flg=True
 for i in list:
-    amari.append(n%i)
-for i in list:
-    if(i == 0):
-        print("is 0")
-print(amari)
-if 0 in amari==True:
-    print("素数ではありません")
-elif 0 in amari==False:
-    print("素数です")
-    """
+    if n%i==0:
+        flg=False
+print(flg)
+
 
 
 
