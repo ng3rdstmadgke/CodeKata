d2ce364f18f3574b1f10e6811cd8e51842ff11b9
HiranoKa <Hirano.katsumi@leadinge.co.jp>
2015-07-22 14:55:06 +0900

    初期コミット3

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,8 +1,6 @@
 __author__ = 'le-user'
 # -*- coding: utf-8 -*-
 for num in range(1,101):
-    #if num%3==0 and num%5==0:
-    #    print("FizzBuzz")
     if num%3==0:
         print("Fizz")
     if num%5==0:
