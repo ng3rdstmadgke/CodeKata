4647852aaafd425c04d7c7657fe928eee5d8c8c2
HiranoKa <Hirano.katsumi@leadinge.co.jp>
2015-07-23 08:58:19 +0900

    整数対応ver

--- prime.py
+++ prime.py
@@ -1,14 +1,17 @@
 __author__ = 'le-user'
 import sys
 # -*- coding: utf-8 -*-
-n=2
-list=range(2,n)
+input=sys.stdin.readline()
+n=int(input)
 flg=True
-for i in list:
-    if n%i==0:
-        flg=False
+if n<2:
+    flg=False
+elif n>2:
+    list=range(2,n)
+    for i in list:
+        if n%i==0:
+            flg=False
 print(flg)
-
 
 
 
