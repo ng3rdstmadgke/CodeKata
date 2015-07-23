5dff3c3c9e511d50274cb42e276f55f7a34037b1
HiranoKa <Hirano.katsumi@leadinge.co.jp>
2015-07-23 09:20:27 +0900

    正の整数に対応＆1~nまでの全ての数字を判別ver

--- prime.py
+++ prime.py
@@ -1,18 +1,23 @@
 __author__ = 'le-user'
 import sys
 # -*- coding: utf-8 -*-
+
+def prime(n):
+    flg=True
+    if n<2:
+        flg=False
+    elif n>2:
+        list=range(2,n)
+        for i in list:
+            if n%i==0:
+                flg=False
+    return flg
+
 input=sys.stdin.readline()
 n=int(input)
-flg=True
-if n<2:
-    flg=False
-elif n>2:
-    list=range(2,n)
-    for i in list:
-        if n%i==0:
-            flg=False
-print(flg)
-
+for i in range(1,n+1):
+   print(i,end=" ")
+   print(prime(i))
 
 
 
