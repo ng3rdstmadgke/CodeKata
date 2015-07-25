69d8058a1b12bf9bfa70a21fd545d3a84500fe8f
keita <midorikawa.keita@leadinge.co.jp>
2015-07-25 22:49:50 +0900

    #1. fizzbuzz 自分で数値を入力できるver

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,10 +1,15 @@
 __author__ = 'le-user'
 # -*- coding: utf-8 -*-
-for num in range(1,101):
-    if num%3==0:
-        print("Fizz",end="")
-    if num%5==0:
-        print("Buzz",end="")
-    if num%3!=0 and num%5!=0:
-        print(num,end="")
-    print(" ",end="")
+def fizzbuzz(number):
+    for num in range(1,number+1):
+        if num%3==0:
+            print("Fizz",end="")
+        if num%5==0:
+            print("Buzz",end="")
+        if num%3!=0 and num%5!=0:
+            print(num,end="")
+        print(" ",end="")
+
+input=input("数字を入力してください")
+n=int(input)
+print(fizzbuzz(n))
