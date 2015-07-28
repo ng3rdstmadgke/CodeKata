eee59d6365b839a6cb3f11e57a1fba0683faddcd
midorikawakeita <midorikawa.keita@leadinge.co.jp>
2015-07-28 17:38:49 +0900

    test_leap_year_3

--- test_leap_year.py
+++ test_leap_year.py
@@ -5,20 +5,20 @@ import leap_year
 
 class leap_year_TestCase(unittest.TestCase):
     def test_4の倍数(self):
-        leap_year.leap_year(2016)
-        self.assertEqual(True,True)
+        l=leap_year.leap_year(2016)
+        self.assertEqual(l,True)
 
     def test_4の倍数じゃない(self):
-        leap_year.leap_year(89)
-        self.assertEqual(False,False)
+        l=leap_year.leap_year(89)
+        self.assertEqual(l,False)
 
     def test_100の倍数(self):
-        leap_year.leap_year(1900)
-        self.assertEqual(False,False)
+        l=leap_year.leap_year(1900)
+        self.assertEqual(l,False)
 
     def test_400の倍数(self):
-        leap_year.leap_year(2000)
-        self.assertEqual(True,True)
+        l=leap_year.leap_year(2000)
+        self.assertEqual(l,True)
 
 
 if __name__ == '__main__':
