3f55c4d8c888e32e07c10bfce0adf2bd03a897fd
midorikawakeita <midorikawa.keita@leadinge.co.jp>
2015-07-28 17:01:07 +0900

    転置行列　うまく動きません。

--- tenti_gyouretu.py
+++ tenti_gyouretu.py
@@ -1,9 +1,24 @@
 __author__ = 'midorikawa.keita'
-import numpy
-a=numpy.array([[1,2,3],[4,5,6],[7,8,9]])
-b=a.transpose()
-print(b)
+import math
+def input_line(input):
+    list=input.split(" ")
+    lines=math.sqrt(len(list))
+    gyouretu=[]
+    for i in range(lines):
+        gyouretu+=list[i*0:i*lines]
+    print(gyouretu)
 
 
 
 
+list="1 2 3 4 5 6 7 8 9".split(" ")
+print(list)
+lines=int(math.sqrt(len(list)))
+print(lines)
+gyouretu=[]
+for i in range(lines):
+    gyouretu.append(list[i*3:(i+1)*3])
+print(gyouretu)
+print(gyouretu[1][2])
+for i in range(3):
+gyouretu[i]
