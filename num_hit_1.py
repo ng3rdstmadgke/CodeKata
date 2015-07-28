bec288d8e42a3acf9ab88f1b73eaa0a081f9b6a4
midorikawakeita <midorikawa.keita@leadinge.co.jp>
2015-07-28 16:19:26 +0900

    数宛ゲーム_1　mainあり

--- num_hit_1.py
+++ num_hit_1.py
@@ -1,23 +1,40 @@
 __author__ = 'midorikawa.keita'
 import random
 # -*- coding: utf-8 -*-
-
-#ランダムで数字を生成1~100まで
-right_num=random.randint(1,100)
-#正解が出るまでループ処理
-while True:
-    #プレイヤーの数値入力
-    input_num=input("数字を入力してください：")
-    player_num=int(input_num)
+def num_judge(player_num,correct_num):
     #if　入力された数字が正解の場合
-    if player_num==right_num:
-        break
+    if player_num==correct_num:
+        flg=0
     #elif　入力された数字が正解より大きい場合
-    elif player_num>right_num:
-        print(str(player_num)+"は正解よりも大きい数字です")
+    elif player_num>correct_num:
+        flg=1
     #elif　入力された数字が正解より小さい場合
-    elif player_num<right_num:
-        print(str(player_num)+"は正解よりも小さい数字です")
+    elif player_num<correct_num:
+        flg=2
+    return flg
 
-#出力→（正解です）
-print("正解です!!")
+if __name__=="__main__":
+    #ランダムで数字を生成1~100まで
+    correct_num=random.randint(1,100)
+    #正解が出るまでループ処理
+    while True:
+        #プレイヤーの数値入力
+        player_num=int(input("数字を入力してください："))
+
+
+        #数値があっているかの判断
+        flg=num_judge(player_num,correct_num)
+
+
+        #if　入力された数字が正解の場合
+        if flg==0:
+            break
+        #elif　入力された数字が正解より大きい場合
+        elif flg==1:
+            print(str(player_num)+"は正解よりも大きい数字です")
+        #elif　入力された数字が正解より小さい場合
+        elif flg==2:
+            print(str(player_num)+"は正解よりも小さい数字です")
+
+    #出力→（正解です）
+    print("正解です!!")
