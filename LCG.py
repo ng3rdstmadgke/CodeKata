79fc011e5856e033fb0ee764635cd0530391104e
midorikawakeita <midorikawa.keita@leadinge.co.jp>
2015-07-28 16:06:42 +0900

    線形合同法　main付き

--- LCG.py
+++ LCG.py
@@ -1,14 +1,25 @@
 __author__ = 'midorikawa.keita'
 # -*- coding: utf-8 -*-
 
-x=12345
-j=0
-for i in range(100):
-    x=(997*x+1)%65536
-    xx=x/100000
-    print(xx)
-    j+=xx
+def my_lcg(n):
+    x=12345
+    list=[]
+    for i in range(n):
+        x=(997*x+1)%65536
+        xx=x/100000
+        list.append(xx)
+    return list
 
-print("平均="+str(j/100))
+if __name__=="__main__":
+    xx=my_lcg(100)
+    j=0
+    for i in range(100):
+        if i%10==0:
+            print(xx[i])
+        else:
+            print(xx[i],end=" ")
+        j+=xx[i]
+
+    print("\n平均="+str(j/100))
 
 
