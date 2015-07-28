bd4dedb40d443b15300205601890fa0e7916b2f5
midorikawakeita <midorikawa.keita@leadinge.co.jp>
2015-07-28 19:33:56 +0900

    転置行列　関数完成

--- tenti_gyouretu.py
+++ tenti_gyouretu.py
@@ -1,24 +1,30 @@
 __author__ = 'midorikawa.keita'
 import math
-def input_line(input):
+def make_gyo(input):
     list=input.split(" ")
-    lines=math.sqrt(len(list))
+    lines=int(math.sqrt(len(list)))
     gyouretu=[]
     for i in range(lines):
-        gyouretu+=list[i*0:i*lines]
-    print(gyouretu)
+        gyouretu.append(list[i*3:(i+1)*3])
+    return gyouretu
+
+def tenti_gyo(gyouretu):
+    gyo_a=[]
+    gyo_b=gyouretu
+    n=len(gyouretu)
+    for i in range(n):
+        for j in range(n):
+            gyo_a.append(gyo_b[j][i])
+    return gyo_a
 
 
+def print_gyo(list):
+        for j in range(len(list)):
+            if int(list[j])<10:
+                print(" {} ".format(list[j]),end="")
+            else:
+                print("{} ".format(list[j]),end="")
+            if (j+1)%(len(list)**0.5)==0:
+                print("")
 
-
-list="1 2 3 4 5 6 7 8 9".split(" ")
-print(list)
-lines=int(math.sqrt(len(list)))
-print(lines)
-gyouretu=[]
-for i in range(lines):
-    gyouretu.append(list[i*3:(i+1)*3])
-print(gyouretu)
-print(gyouretu[1][2])
-for i in range(3):
-gyouretu[i]
+#if __name__=="__main__"
