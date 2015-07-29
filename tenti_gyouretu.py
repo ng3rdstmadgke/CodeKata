621aefc158d393e37be636e59ab72baa70a7e421
midorikawakeita <midorikawa.keita@leadinge.co.jp>
2015-07-29 09:38:15 +0900

    転置行列　mainあり

--- tenti_gyouretu.py
+++ tenti_gyouretu.py
@@ -1,13 +1,15 @@
 __author__ = 'midorikawa.keita'
-import math
+import sys
+#入力内容を行列の形式にする
 def make_gyo(input):
     list=input.split(" ")
-    lines=int(math.sqrt(len(list)))
+    lines=int((len(list))**0.5)
     gyouretu=[]
     for i in range(lines):
-        gyouretu.append(list[i*3:(i+1)*3])
+        gyouretu.append(list[i*lines:(i+1)*lines])
     return gyouretu
 
+#受け取った行列を転置してリストで返す
 def tenti_gyo(gyouretu):
     gyo_a=[]
     gyo_b=gyouretu
@@ -17,7 +19,7 @@ def tenti_gyo(gyouretu):
             gyo_a.append(gyo_b[j][i])
     return gyo_a
 
-
+#出来上がった転置リストを行列の形式で表示
 def print_gyo(list):
         for j in range(len(list)):
             if int(list[j])<10:
@@ -27,4 +29,9 @@ def print_gyo(list):
             if (j+1)%(len(list)**0.5)==0:
                 print("")
 
-#if __name__=="__main__"
+
+if __name__=="__main__":
+    input_line=sys.stdin.readline()
+    gyouretu=make_gyo(input_line)
+    gyouretu_tenti=tenti_gyo(gyouretu)
+    print_gyo(gyouretu_tenti)
