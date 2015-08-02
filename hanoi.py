a3479caf4ff552239635b5f10f70df2b7fa865b3
midorikawa <midorikawa.keita@leadinge.co.jp>
2015-08-02 22:01:23 +0900

    ハノイの塔　Aの塔をCに移動

--- hanoi.py
+++ hanoi.py
@@ -1,11 +1,13 @@
 __author__ = 'midorikawa.keita'
 # -*- coding: utf-8 -*-
-#n個の円盤を用意
-#a,b,cの柱を用意
-#1.　n - 1 個目までの円盤をaからcに移動する。
-#2.　n番目の円盤をaからcに移動する。
-#3.　n-2番目までの円盤をbからcに移動する
-#4.　n-1番目の円盤をcに移動
-#5.　以下#1～#4の繰り返し
+
+def my_hanoi(n,a,c,b):
+    if n>0:
+        my_hanoi(n-1,a,b,c)
+        print("{0}を{1}から{2}に移動".format(n,a,c))
+        my_hanoi(n-1,b,c,a)
+
+if __name__=="__main__":
+    my_hanoi(3,"A","C","B")
 
 
