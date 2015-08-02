8374f25b9f1fb2c1863105425c4b602800946fae
midorikawa <midorikawa.keita@leadinge.co.jp>
2015-08-03 02:13:43 +0900

    ギャスケット　再起　正常に動作

--- my_gasket.py
+++ my_gasket.py
@@ -4,39 +4,42 @@ def sankaku(n,t):
     for i in range(3):
         t.fd(n)
         t.left(120)
-#    t.goto(n/4,(n/4)*(3**0.5))
-#    for i in range(3):
-#        t.fd(n/2)
-#        t.right(120)
 
-def go_point(x,y,t):
-    t.penup()
-    t.goto(x,y)
-    t.pendown()
 
 def gasket(n,length,t):
     if n>1:
         gasket(n-1,length,t)
-        go_point(length*(n-2),0,t)
-        t.fd(length*2**(n-1))
-        gasket(n-1,length,t)
-        go_point(length*(n-2),0,t)
-        t.left(60)
-        t.fd(length*2**(n-1))
+        t.fd(length)
         t.right(60)
+        t.fd(length*2**(n-1)-length)
+        t.left(60)
+        gasket(n-1,length,t)
+        t.left(60)
+        t.fd(length)
+        t.left(120)
+        t.fd(length*2**(n-1))
+        t.right(180)
         gasket(n-1,length,t)
     if n==1:
-        sankaku(length,t)
+        for i in range(2):
+            t.fd(length*2)
+            t.left(120)
         t.fd(length)
-        sankaku(length,t)
+        t.left(120)
+        for i in range(2):
+            t.fd(length)
+            t.right(120)
+        t.fd(length)
         t.left(120)
         t.fd(length)
-        t.right(120)
-        sankaku(length,t)
+        t.left(180)
+        t.fd(length)
+        t.right(60)
+
 
 
 
 if __name__=="__main__":
     t=turtle.Turtle()
-    gasket(3,10,t)
+    gasket(5,10,t)
 
