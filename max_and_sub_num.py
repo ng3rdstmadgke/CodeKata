b32873767d02df484cb084646d2d1234c343f46f
midorikawakeita <midorikawa.keita@leadinge.co.jp>
2015-08-03 14:11:38 +0900

    最大値と添字を表示　リストの中の８つの数字の最大値を取り出し、削除する作業を３回繰り返す。

--- max_and_sub_num.py
+++ max_and_sub_num.py
@@ -11,4 +11,14 @@ def max_sub_num():
         num_list.append(n)
         st_list.remove(n)
     print(num_list)
-    #一番大きい数字を探し、その数字と添字を表示し、num_listの数字をに置換する
+    #一番大きい数字を探し、その数字と添字を表示し、num_listのその数字を0に置換する
+    answer=[]
+    for i in range(3):
+        max_num=max(num_list)
+        sub_num=num_list.index(max_num)
+        print(str(sub_num)+"->"+str(max_num))
+        num_list[sub_num]=0
+    return answer
+
+if __name__=="__main__":
+    max_sub_num()
