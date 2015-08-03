1e5a6e44118a5da56ba0921c9e6166756906f24e
midorikawakeita <midorikawa.keita@leadinge.co.jp>
2015-08-03 11:06:48 +0900

    干支判定　正常に動作

--- my-eto.py
+++ my-eto.py
@@ -5,9 +5,9 @@ def my_eto(year):
     #干支のリストを作る
     eto=list("子丑寅卯辰巳午未申酉戌亥")
     #西暦-4を１２で割ってあまりで干支リストをインデックスする
-    t=(int(year)-4)%12
+    t=(year-4)%12
     return eto[t]
 
 if __name__=="__main__":
-    year=sys.stdin.readline()
-    print(my_eto(year))
+    year=int(sys.stdin.readline().rstrip())
+    print("{}年は{}年です".format(year,my_eto(year)))
