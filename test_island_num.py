9ec73ac1f8297cb9878d3cf08abc64ec2a8312f3
midorikawakeita <midorikawa.keita@leadinge.co.jp>
2015-09-04 16:18:38 +0900

    初期化メソッドのテストを作成　結果ＯＫ

---
+++ test_island_num.py
@@ -0,0 +1,22 @@
+__author__ = 'midorikawakeita'
+
+import unittest
+import island_num
+
+
+class MyTestCase(unittest.TestCase):
+    def test_初期化メソッド(self):
+        island_init=island_num.my_island("5 6 ##...##.#...#..###.....#.###..")
+        print("行数(height)：{}".format(island_init.height))
+        print("列数(width)：{}".format(island_init.width))
+        print("入力文字列(map_str)：{}".format(island_init.map_str))
+        print("入力内容で行列化(map_list)：{}".format(island_init.map_list))
+        self.assertEqual(island_init.height , 5)
+        self.assertEqual(island_init.width , 6)
+        self.assertEqual(island_init.map_str , "##...##.#...#..###.....#.###..")
+        self.assertEqual(island_init.map_list , [['#', '#', '.', '.', '.', '#'], ['#', '.', '#', '.', '.', '.'], ['#', '.', '.', '#', '#', '#'], ['.', '.', '.', '.', '.', '#'], ['.', '#', '#', '#', '.', '.']])
+
+
+
+if __name__ == '__main__':
+    unittest.main()
