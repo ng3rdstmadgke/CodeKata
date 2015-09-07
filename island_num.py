4a4e58e2c7d0ffe2daab21e5cc14548bd01a09d9
midorikawakeita <midorikawa.keita@leadinge.co.jp>
2015-09-07 11:23:07 +0900

    正常に動作　＋を発見したらその上下左右の＃を＋に置換することを繰り返して、島を検知する。

--- island_num.py
+++ island_num.py
@@ -1,18 +1,29 @@
 __author__ = 'midorikawakeita'
 
 """
-5 6 ##...##.#...#..###.....#.###..
+5 6 .#...####...#.####.....#.###..
 
 5 6
-##...#
-#.#...
-##.##.
-...##.
-.##...
-.#..##
+.#...#
+###...
+#.####
 .....#
-"""
-"""
+.###..
+
+
+10 10 .#...####...#.####...###.#...........##..#...####.###..##.#.#.#..##.#.#.##.##.#..#.........#...####.
+
+.#...####.
+..#.####..
+.###.#....
+.......##.
+.#...####.
+###..##.#.
+#.#..##.#.
+#.##.##.#.
+.#........
+.#...####.
+
 準備１
 １与えられた入力を行列にする
 
@@ -23,14 +34,10 @@ __author__ = 'midorikawakeita'
 処理２
 １左上からひとつずつ文字を読み込む
 ２＋を読み込んだら、その上下左右を読み込み＃なら＋に変換する
+３＋の周りの＃がなくなるまで繰り返す
 ３処理３に移行
 
-処理３
-１右下からひとつずつ文字を読み込む
-２＋を読み込んだら、その上下左右を読み込み＃なら＋に変換する
-３処理４に移行
-
-処理４
+処理3
 １左上からひとつずつ文字を読み込み
 ２読み込んだ文字が＋だったら．に変換
 ３処理１に戻る
@@ -51,37 +58,71 @@ class my_island:
 
 
     """処理１：リストを順に読み込み「#」を「+」に変換。変換後のリストを戻す"""
-    def is_rec(self):
+    def is_rec(self,map_list):
         for i in range(self.height):
             for j in range(self.width):
-                masu=self.map_list[i][j]
+                masu = map_list[i][j]
                 if masu == "#" :
-                    self.map_list[i][j] = "+"
-                    return self.map_list
+                    map_list[i][j] = "+"
+                    return map_list
+        return 1
 
-    """処理２：処理１で変換したリストを引数にとる。左上から順に読み込み、＋を読み込んだら上下左右のマスを＋に変換"""
+    """処理２：処理１で変換したリストを引数にとる。左上から順に読み込み、＋を読み込んだら左右上下の"#"を"+"に変換"""
+    def left(self,map_list):
+        while True:
+            break_count=0
+            for i in range(self.height):
+                for j in range(self.width):
+                    masu = map_list[i][j]
+                    if masu == "+":
+                        if j != (self.width - 1):
+                            if map_list[i][j + 1] == "#":
+                                map_list[i][j + 1] = "+"
+                                break_count += 1
+                        if i != self.height-1:
+                            if map_list[i + 1][j] == "#":
+                                map_list[i + 1][j] = "+"
+                                break_count += 1
+                        if j != 0:
+                            if map_list[i][j - 1] == "#":
+                                map_list[i][j - 1] = "+"
+                                break_count += 1
+                        if i != 0:
+                            if map_list[i - 1][j] == "#":
+                                map_list[i - 1][j] = "+"
+                                break_count += 1
+            if break_count == 0:
+                break
+        return map_list
 
 
+    """処理3：処理３で変換したリストを引数にとる。左上から順に読み込み、＋を読み込んだら.に変換"""
+    def is_del(self,map_list):
+        for i in range(self.height):
+            for j in range(self.width):
+                if map_list[i][j] == "+":
+                    map_list[i][j] = "."
+        return map_list
 
-
-
-
-
-
+    """処理１〜処理3までを組み合わせて、島の数をカウントする"""
+    def main(self):
+        count = 0
+        map = self.map_list
+        while True:
+            map_a = self.is_rec(map)
+            if map_a == 1:
+                break
+            map_b = self.left(map_a)
+            map = self.is_del(map_b)
+            count += 1
+        return count
 
 
 
 
 
 if __name__ == "__main__":
-    my_str = "ABCDEFGHIJKL"
-    ll = []
-    for i in range(len(my_str) // 3):
-        st=i * 3
-        ed=(i + 1) * 3
-        ll.append(my_str[st:ed])
-    my_list = list(my_str)
-    my_list[3] = 5
-    print(my_str)
-    print(my_list)
-    print(ll)
+    island_init = my_island("10 10 .#...####...#.####...###.#...........##..#...####.###..##.#.#.#..##.#.#.##.##.#..#.........#...####.")
+    num = island_init.main()
+    print("島の数：{}".format(num))
+
