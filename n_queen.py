8bbaa4f609e64832cae55047c30fd4db033ddca4
midorikawakeita <midorikawa.keita@leadinge.co.jp>
2015-09-11 18:00:26 +0900

    初期コミット

--- n_queen.py
+++ n_queen.py
@@ -1,4 +1,5 @@
 __author__ = 'midorikawakeita'
+import random
 """
 ....... ....... ..Q.... ....... ...Q... ....... .......
 
@@ -30,24 +31,63 @@ class N_queen:
         self.width = len(self.base_map[0])
         self.height = len(self.base_map)
 
-    def search_q(self,mapping):
+    """Qを探してQが動く範囲をぬりつぶす"""
+    def check(self,mapping):
         for i in range(self.height):
             for n in range(self.width):
                 if mapping[i][n] == "Q":
-                    mapping[i][n] = "A"
                     #左右を＃に置換
                     mapping[i] = list("#" * self.width)
                     #上下を＃に置換
                     for h in range(self.height):
                         mapping[h][n] = "#"
-                    #右斜めを＃に置換
+                    #斜めを＃に置換
+                    y = 1
+                    x = 1
                     while True:
-                        y = 1
-                        x = 1
-                        if ((i - y) >= 0) and ((n + x) <= self.width):#右斜め上
-                            mapping[i - y][n + x]
-                    #左斜めを＃に置換
-                    return i,n
+                        count = 0
+                        if ((i - y) >= 0) and ((n + x) <= self.width - 1):#右斜め上
+                            mapping[i - y][n + x] = "#"
+                            count += 1
+                        if ((i + y) <= (self.height - 1)) and ((n + x) <= (self.width - 1)):#右斜め下
+                            mapping[i + y][n + x] = "#"
+                            count += 1
+                        if ((i - y) >= 0) and ((n - x) >= 0):#左斜め上
+                            mapping[i - y][n - x] = "#"
+                            count += 1
+                        if ((i + y) <= (self.height -1)) and ((n -x) >= 0):#左斜め下
+                            mapping[i + y][n - x] = "#"
+                            count += 1
+                        y += 1
+                        x += 1
+                        if count == 0:
+                            mapping[i][n] = "q"
+                            break
+        return mapping
+
+    """ランダムで１００回"""
+    def search_path(self,c_map):
+        map_list = []
+        qnn = 0
+        for e in range(5):
+            for n in [0,1,3,5,6]:
+                count_1 = []
+                for i in range(self.width):
+                    if c_map[n][i] == ".":
+                        count_1.append(i)
+                if count_1:
+                    num = random.choice(count_1)
+                    c_map[n][num] = "Q"
+                else:
+                    pass
+            random_map = self.check(c_map)
+            map_list += "".join(["".join(i) for i in random_map])
+        for i in range(len(map_list)):
+            qn = map_list[i].count("q")
+            if qn > qnn:
+                qnn = qn
+                i_num = i
+        return map_list[i_num]
 
 
 
